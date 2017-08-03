eb11d50224edfcfd40aed99e27f043d0f98b2363
Reza <rezabny@gmail.com>
2017-08-03 10:32:54 +1000

    hhjh

---
+++ epilepsydataprovider/SeizureDataRead.py
@@ -0,0 +1,20 @@
+import signalprocessingbank
+
+def read_chbmit():
+    return None
+
+
+def read_kaggle_2014(data_address, folders='all'):
+    if folders == 'all':
+        pass
+        # Read all data
+    else:
+        # Read the data for the given participant
+        pass
+
+    return None
+
+def read_Freiburg():
+    return None
+
+
