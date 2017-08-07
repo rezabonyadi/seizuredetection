020031864bd4aa916e2c583fa0f0d40ef93622f9
Reza <rezabny@gmail.com>
2017-08-07 15:41:49 +1000

    hhjh

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from epilepsydataprovider import SeizureDataRead
 import RawDataModels
 import numpy as np
+import json
 
-kaggle_address = "R:/projects/Seizure/Seizure_detection"
+
+with open('SETTINGS.json') as f:
+    settings = json.load(f)
+
+kaggle_address = str(settings['kaggle-data-dir'])
+
 folders = ["Dog_1"]
 data = SeizureDataRead.read_kaggle_2014(kaggle_address, folders[0], 0.8)
 train_in, train_out = SeizureDataRead.prepare_data(data)
