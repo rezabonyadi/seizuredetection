e9eb00293ce400ec5373eb57c31f6de29b3f6fd6
rezabonyadi <Reza@1983>
2017-08-09 08:45:30 +1000

    j

--- RawDataModels.py
+++ RawDataModels.py
@@ -13,24 +13,41 @@ import keras.backend as K
 def roc_auc(y_true, y_pred):
     return roc_auc_score(y_true, y_pred)
 
+
+# def my_print_fcn(x):
+    # print(x)
+
+
 def cnn_1d(train_in, train_out, test_in, test_out):
     n_channels = int(train_in[0].shape[1])
     n_samples = int(train_in[0].shape[0])
     train_out = to_categorical(train_out)
     test_out = to_categorical(test_out)
 
+    # model = Sequential()
+    # model.add(Conv1D(n_channels * 2, 10, input_shape=(n_samples, n_channels)))
+    # model.add(Conv1D(n_channels * 2, 10, activation='tanh'))
+    # model.add(LeakyReLU(alpha=0.3))
+    # model.add(Conv1D(n_channels * 2, 5, activation='tanh'))
+    # model.add(Conv1D(n_channels * 2, 10))
+    # model.add(LeakyReLU(alpha=0.3))
+    # model.add(Flatten())
+    # model.add(Dense(64, activation='relu'))
+    # model.add(Dropout(0.5))
+    # model.add(Dense(2, activation='softmax'))
+    # model.summary(print_fn=my_print_fcn)
+
     model = Sequential()
     model.add(Conv1D(n_channels * 2, 10, input_shape=(n_samples, n_channels)))
     model.add(Conv1D(n_channels * 2, 10, activation='tanh'))
-    model.add(LeakyReLU(alpha=0.3))
-    model.add(Conv1D(n_channels * 2, 5, activation='tanh'))
+    # model.add(LeakyReLU(alpha=0.3))
+    model.add(Conv1D(n_channels * 2, 5, activation='relu'))
     model.add(Conv1D(n_channels * 2, 10))
-    model.add(LeakyReLU(alpha=0.3))
+    # model.add(LeakyReLU(alpha=0.3))
     model.add(Flatten())
     model.add(Dense(64, activation='relu'))
     model.add(Dropout(0.5))
     model.add(Dense(2, activation='softmax'))
-    # model.summary()
 
     model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
     model.fit(train_in, train_out, epochs=200, verbose=0)
@@ -42,6 +59,47 @@ def cnn_1d(train_in, train_out, test_in, test_out):
     # print("")
     auc_test = roc_auc(test_out[:, 0], test_scores[:, 0])
     auc_train = roc_auc(train_out[:, 0], train_scores[:, 0])
-    print('AUC test %f AUC train %f' %(auc_test, auc_train))
+    print('Final results: AUC test %f AUC train %f' %(auc_test, auc_train))
+    print('*******************************************************************')
     return model, auc_train, auc_test
 
+
+def cnn_2d(train_in, train_out, test_in, test_out):
+    n_instances_train = len(train_in)
+    n_instances_test = len(test_in)
+    n_channels = int(train_in[0].shape[1])
+    n_samples = int(train_in[0].shape[0])
+    train_out = to_categorical(train_out)
+    test_out = to_categorical(test_out)
+
+    train_in = np.reshape(train_in, (n_instances_train, n_samples, n_channels, 1))
+    test_in = np.reshape(test_in, (n_instances_test, n_samples, n_channels, 1))
+
+    model = Sequential()
+    model.add(Conv2D(n_channels * 2, (10, n_channels), input_shape=(n_samples, n_channels, 1)))
+    model.add(Reshape((-1, n_channels * 2, 1)))
+    model.add(Conv2D(n_channels * 2, (10, n_channels * 2), activation='tanh'))
+    model.add(LeakyReLU(alpha=0.3))
+    model.add(Reshape((-1, n_channels * 2)))
+    model.add(Conv1D(n_channels * 2, 5, activation='tanh'))
+    model.add(Conv1D(n_channels * 2, 10))
+    model.add(LeakyReLU(alpha=0.3))
+    model.add(Flatten())
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(2, activation='softmax'))
+    # model.summary(print_fn=my_print_fcn)
+
+    model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+    model.fit(train_in, train_out, epochs=200, verbose=0)
+    # score = model.evaluate(test_in, test_out, batch_size=32,verbose=0)
+    # print("")
+    # print(score)
+    train_scores = model.predict_proba(train_in, verbose=0)
+    test_scores = model.predict_proba(test_in, verbose=0)
+    # print("")
+    auc_test = roc_auc(test_out[:, 0], test_scores[:, 0])
+    auc_train = roc_auc(train_out[:, 0], train_scores[:, 0])
+    print('Final results: AUC test %f AUC train %f' %(auc_test, auc_train))
+    print('*******************************************************************')
+    return model, auc_train, auc_test
--- main.py
+++ main.py
@@ -1,29 +1,58 @@
 from epilepsydataprovider import SeizureDataRead
 import RawDataModels
+import os
+from time import strftime, localtime
 import numpy as np
 import json
 import csv
 
+
+def save_details(model_details, res_details, folder):
+    if not os.path.exists('results/' + folder):
+        os.makedirs('results/' + folder)
+
+    c_time = strftime('%Y-%m-%d-%H-%M-%S', localtime())
+    f_name = 'results/%s/models-%s.json' % (folder, c_time)
+
+    with open(f_name, 'w') as fp:
+        json.dump(model_details, fp, indent=4)
+
+    f_name = 'results/%s/res-%s.csv' % (folder, c_time)
+
+    with open(f_name, 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(('subject', 'auc_train', 'auc_test'))
+        [writer.writerow(r) for r in res_details]
+
+
+def kaggle_data_2014_patient_specific(address):
+    folders = ["Dog_1", "Dog_2", "Dog_3", "Dog_4", "Patient_1", "Patient_2", "Patient_3", "Patient_4", "Patient_5",
+               "Patient_6", "Patient_7", "Patient_8"]
+    folders = ["Dog_2"]
+    model_details = dict()
+
+    res_details = []
+
+    for subject in folders:
+        data = SeizureDataRead.read_kaggle_2014(address, subject, .3)
+        print("Modeling of %s in progress" %subject)
+        train_in, train_out, train_lat, test_in, test_out, test_lat = SeizureDataRead.prepare_data(data)
+        # train_out = np.reshape(data["labels"], (n_instances))
+        model, acc_train, acc_test = RawDataModels.cnn_1d(train_in, train_out, test_in, test_out)
+        # model, acc_train, acc_test = RawDataModels.cnn_2d(train_in, train_out, test_in, test_out)
+        model_details[subject] = model.to_json()
+
+        res_details.append((subject, acc_train, acc_test))
+
+    save_details(model_details, res_details, 'kaggle_2014')
+
+
 with open('SETTINGS.json') as f:
     settings = json.load(f)
-
 kaggle_address = str(settings['kaggle-data-dir'])
+if not os.path.exists('results'):
+    os.makedirs('results')
 
-folders = ["Dog_1", "Dog_2", "Dog_3", "Dog_4", "Patient_1", "Patient_2", "Patient_3", "Patient_4", "Patient_5",
-           "Patient_6", "Patient_7", "Patient_8"]
-# folders = ["Patient_1"]
-model_details = dict()
-
-for subject in folders:
-    data = SeizureDataRead.read_kaggle_2014(kaggle_address, subject, .3)
-    print("Modeling of %s in progress" %subject)
-    train_in, train_out, train_lat, test_in, test_out, test_lat = SeizureDataRead.prepare_data(data)
-    # train_out = np.reshape(data["labels"], (n_instances))
-    model, acc_train, acc_test = RawDataModels.cnn_1d(train_in, train_out, test_in, test_out)
-    # model_details[subject + "-model"] = model.to_json()
-    model_details[subject + "-auc"] = (acc_train, acc_test)
-
-with open('result.json', 'w') as fp:
-    json.dump(model_details, fp, indent=4)
+kaggle_data_2014_patient_specific(kaggle_address)
 
 i = 0
